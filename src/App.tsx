3142d96fc6cc19601f08ce9e6a5a57d03f514bda
Osama <shaikhosama499@gmail.com>
2024-11-07 00:13:19 +0530

    build(root): chakra ui added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col gap-20'>
+      <div className='flex flex-col gap-16 md:gap-28'>
         <Home />
         <About />
         <LatestArticles />
